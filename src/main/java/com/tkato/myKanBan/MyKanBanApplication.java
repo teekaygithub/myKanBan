8f4fb234af6e4e198d12dfef4390f3494a45a1e2
teekaygithub <tomoyuki.kato@yahoo.com>
2021-03-07 17:45:09 -0800

    initial commit

---
+++ src/main/java/com/tkato/myKanBan/MyKanBanApplication.java
@@ -0,0 +1,13 @@
+package com.tkato.myKanBan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKanBanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKanBanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkato/myKanBan/MyKanBanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkato.myKanBan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKanBanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
