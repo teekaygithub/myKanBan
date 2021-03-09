7dd1cf73d4d6bc66133b416acdb5448292062657
teekaygithub <tomoyuki.kato@yahoo.com>
2021-03-09 13:41:18 -0800

    Property file for test-specific MySQL database. Changed controller test to be an integration test instead of unit-test (where controller was mocked)

--- src/test/java/com/tkato/myKanBan/controller/MainControllerTest.java
+++ src/test/java/com/tkato/myKanBan/controller/MainControllerTest.java
@@ -1,9 +1,8 @@
 package com.tkato.myKanBan.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.tkato.myKanBan.model.Project;
@@ -12,15 +11,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
-import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(
+    locations = "classpath:application-test.properties"
+)
 public class MainControllerTest {
 
     @LocalServerPort
@@ -29,23 +32,39 @@ public class MainControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @MockBean
-    private MainController mockRepository;
-
     @Test
-    public void addNewProjectTest() throws Exception {
+    public void addNewProjectTest() {
         Project test = new Project();
-        test.setTitle("testing");
-        List<Project> projects = new ArrayList<>();
-        projects.add(test);
+        test.setTitle("test project");
 
-        when(mockRepository.getAllProjects()).thenReturn(projects);
-
-        ResponseEntity<String> response = restTemplate.getForEntity(
-            new URL("http://localhost:" + port + "/api/all").toString(), String.class
+        ResponseEntity<String> res = restTemplate.postForEntity(
+            "http://localhost:" + port + "/api/addproject", test, String.class
         );
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(HttpStatus.CREATED, res.getStatusCode());
+    }
+
+    @Test
+    public void addNewProjectBadRequest() throws Exception {
+        Project test = new Project();
+
+        ResponseEntity<String> res = restTemplate.postForEntity(
+            "http://localhost:" + port + "/api/addproject", test, String.class);
         
+        assertNotEquals(HttpStatus.OK, res.getStatusCode());
+        assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
+	}
+
+    @Test
+    public void getAllProjectsTest() {
+        ResponseEntity<List<Project>> res = restTemplate.exchange(
+            "http://localhost:" + port + "/api/all", 
+            HttpMethod.GET, 
+            null, 
+            new ParameterizedTypeReference<List<Project>>(){}
+        );
+
+        assertEquals(HttpStatus.OK, res.getStatusCode());
+        System.out.format("len: %d\n", res.getBody().size());
     }
 }
