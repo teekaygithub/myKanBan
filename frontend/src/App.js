8bd7461d223f49954e68ccc8e124bd7988cab465
teekaygithub <tomoyuki.kato@yahoo.com>
2021-03-15 23:05:38 -0700

    Basic login and logout functionality. Added a login page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,74 @@
 import './App.css';
-import Header from "./components/Header";
-import ProjectContainer from "./components/ProjectContainer";
-import KanBanContainer from "./components/KanBanContainer";
+import React, {Component} from 'react';
 import {
   Switch,
   Route,
+  Redirect
 } from 'react-router-dom'
+import Header from "./components/Header";
+import ProjectContainer from "./components/ProjectContainer";
+import KanBanContainer from "./components/KanBanContainer";
 import Welcome from './components/Welcome';
 import Footer from './components/Footer';
+import Login from './components/Login';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      loggedIn:false
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogin(email, pw) {
+    if (email === 'test@gmail.com') {
+      this.setState({
+        loggedIn: true
+      });
+      console.log(`state.loggedIn: ${this.state.loggedIn}`);
+    } else {
+      console.log(`INVALID EMAIL: ${email}`);
+    }
+  }
+  
+  handleLogout() {
+    this.setState({
+      loggedIn:false
+    });
+  }
+
+  render() {
+    return (
       <div className="App">
-        <Header />
+        <Header loggedIn={this.state.loggedIn} logoutHandler={this.handleLogout} />
         <Switch>
+
           <Route exact path="/">
             <Welcome />
           </Route>
-          <Route exact path = "/project/:id" component={KanBanContainer} />
-          <Route path="/projects">
-            <ProjectContainer />
+          
+          <Route path="/login">
+            <Login loginHandler={this.handleLogin} />
           </Route>
+          
+          <Route exact path = "/project/:id" component={KanBanContainer} />
+          
+          <Route path="/projects">
+            {console.log(`state.loggedIn: ${this.state.loggedIn}`)}
+            {this.state.loggedIn ? <ProjectContainer /> : <Redirect to="/login" />}
+          </Route>
+          
           <Route path="/about">
             <About />
           </Route>
+
         </Switch>
         <Footer />
       </div>
-  );
+    );
+  }
 }
 
 function About () {
