8e5b763f5b8eab49a2c9c8ed4cfea13538dbd486
teekaygithub <tomoyuki.kato@yahoo.com>
2021-03-29 00:09:46 -0700

    Frontend: split the header menu separately (authenticated/non-authenticated) for better organization.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,39 +1,40 @@
 import {Link} from 'react-router-dom';
 
 function Header (props) {
-  if (props.loggedIn) {
-    return (
-      <nav className="navbar navbar-expand-sm bg-dark">
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to="/projects" className="nav-link">Projects</Link>
-          </li>
-          <li className='nav-item'>
-            <a href="" onClick={props.logoutHandler}  className="nav-link">Log Out</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  } else {
-    return (
-      <nav className="navbar navbar-expand-sm bg-dark">
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to="/" className="nav-link">Register</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to="/login" className="nav-link">Log In</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+
+  const authMenu = (
+    <>
+      <li className='nav-item'>
+        <Link to="/" className="nav-link">Home</Link>
+      </li>
+      <li className='nav-item'>
+        <Link to="/projects" className="nav-link">Projects</Link>
+      </li>
+      <li className='nav-item'>
+        <a href="" onClick={props.logoutHandler}  className="nav-link">Log Out</a>
+      </li>
+    </>);
+
+  const nonAuthMenu = (
+    <>
+      <li className='nav-item'>
+        <Link to="/" className="nav-link">Home</Link>
+      </li>
+      <li className='nav-item'>
+        <Link to="/" className="nav-link">Register</Link>
+      </li>
+      <li className='nav-item'>
+        <Link to="/login" className="nav-link">Log In</Link>
+      </li>
+    </>);
+
+  return (
+    <nav className="navbar navbar-expand-sm bg-dark">
+      <ul className="navbar-nav">
+        {props.loggedIn ? authMenu: nonAuthMenu}
+      </ul>
+    </nav>
+  );
 }
 
 export default Header;
