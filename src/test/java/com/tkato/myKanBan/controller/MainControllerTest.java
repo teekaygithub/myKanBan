b6497aedd7319d575b4254041aaef28dabdc854e
teekaygithub <tomoyuki.kato@yahoo.com>
2021-11-05 22:46:06 -0700

    Added spring security test dependency for MVC mocking. Security integration test for obtaining JWT by logging in.

--- src/test/java/com/tkato/myKanBan/controller/MainControllerTest.java
+++ src/test/java/com/tkato/myKanBan/controller/MainControllerTest.java
@@ -1,39 +1,85 @@
 package com.tkato.myKanBan.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import java.util.List;
-
-import com.tkato.myKanBan.model.Project;
-import com.tkato.myKanBan.model.Ticket;
-import com.tkato.myKanBan.security.JwtUtility;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(/*webEnvironment = WebEnvironment.RANDOM_PORT*/)
 @TestPropertySource(
     locations = "classpath:application-test.properties"
 )
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 public class MainControllerTest {
 
-    @LocalServerPort
-    private int port;
+    private MockMvc mvc;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
+    @Test
+    public void registerUserTest() throws Exception {
+        String username = "batman@test.com";
+        String fullname = "Bruce Wayne";
+        String password = "martha";
+        String body = "{\"username\":\"" + username + "\", \"fullname\":\"" + fullname + "\", \"password\":\"" + password + "\"}";
+
+        MvcResult result = mvc.perform(post("http://localhost:8080/api/users/register").content(body).contentType("application/json"))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+
+        assertEquals(result.getResponse().getStatus(), HttpStatus.CREATED.value());
+    }
+
+    // @Test
+    // public void getAllUsersTest() throws Exception {
+    //     MvcResult result = mvc.perform(get("http://localhost:8080/api/users/accounts")).andExpect(status().isOk()).andReturn();
+
+    //     String resposne = result.getResponse().getContentAsString();
+
+    //     assertEquals(1, 1);
+    // }
+
+    // @Test
+    // @WithMockUser("/someguy")
+    // public void loginTest() throws Exception {
+    //     String username = "batman@test.com";
+    //     String password = "martha";
+    //     String body = "{\"username\":\"" + username + "\", \"password\":\""
+    //               + password + "\"}";
+    //     MvcResult result = mvc.perform(post("http://localhost:8080/api/users/login").content(body).contentType("application/json"))
+    //             .andExpect(status().isOk())
+    //             .andReturn();
+
+    //     String response = result.getResponse().getContentAsString();
+
+    //     // assertEquals(result.getResponse().getStatus(), HttpStatus.OK.value());
+    //     assertEquals(1, 1);
+    // }
 
     /*@Test
     public void addNewProjectTest() {
